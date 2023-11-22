a5e0948348b9dd83ae060f53bea33f97adf51ba0
juan.bravo <juan.bravo@it-and-apps.mx>
2023-11-21 21:27:27 -0600

    update

--- myApp.js
+++ myApp.js
@@ -1,12 +1,10 @@
 let express = require('express');
 let app = express();
 
-let absolutePath = __dirname + "/"
-
 app.get("/", function (req, res) {
     console.log("Hello Express");
-    res.send("Hello Express");
-    res.sendFile(__dirname +"/views/index.html"
+    //res.send("Hello Express");
+    res.sendFile(__dirname +"/views/index.html");
 });
 
 
