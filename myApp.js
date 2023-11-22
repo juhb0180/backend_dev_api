2861e5872b4408dac8784eb49f22be380e638faa
juan.bravo <juan.bravo@it-and-apps.mx>
2023-11-22 14:43:39 -0600

    update

--- myApp.js
+++ myApp.js
@@ -1,18 +1,24 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
 app.get("/", function (req, res) {
     console.log("Hello Express");
     //res.send("Hello Express");
-    res.sendFile(__dirname +"/views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use("/public", express.static(__dirname +"/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", function (req, res) {
     console.log("Hello Express");
     //res.send("Hello Express");
-    res.json({"message":"Hello json"});
+    if (process.env.MESSAGE_STYLE=="uppercase") {
+        res.json({ "message": "Hello json".toUpperCase });
+    } else {
+        res.json({ "message": "Hello json" });
+    }
 });
 
 
