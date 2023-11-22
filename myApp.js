eb450ca41cc9f0f748abc6014f73acac8ca87f3b
juan.bravo <juan.bravo@it-and-apps.mx>
2023-11-22 14:36:12 -0600

    update

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@ app.use("/public", express.static(__dirname +"/public"));
 app.get("/json", function (req, res) {
     console.log("Hello Express");
     //res.send("Hello Express");
-    res.sendFile({"message":"Hello json"});
+    res.json({"message":"Hello json"});
 });
 
 
